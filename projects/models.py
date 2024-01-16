ae9c81411673dec0943294822fda0fb6d9eae35e
Vinícius Souza <tradutorvinicius@gmail.com>
2024-01-16 11:39:06 -0300

    Resolve requisito 6

--- projects/models.py
+++ projects/models.py
@@ -17,7 +17,7 @@ class Project(models.Model):
     github_url = models.URLField()
     keyword = models.CharField(max_length=50)
     key_skill = models.CharField(max_length=50)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects')
 
     def __str__(self):
         return self.name
